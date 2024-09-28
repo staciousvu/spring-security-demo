0942f9cab17645f8566a82c94e4c806850afb079
Vũ Nguyễn Bá <vunguyenba310703@gmail.com>
2024-09-28 09:40:30 +0700

    Update UserInfo.java

--- src/main/java/com/example/jwtsecutirydemo/entity/UserInfo.java
+++ src/main/java/com/example/jwtsecutirydemo/entity/UserInfo.java
@@ -1,2 +1,23 @@
-package com.example.jwtsecutirydemo.entity;public class UserInfo {
+package com.example.jwtsecutirydemo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "userinfo")
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String roles;
+
 }
