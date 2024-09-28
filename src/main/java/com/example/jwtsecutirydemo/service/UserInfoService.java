543303e6ef0cff62251c6f7d39c13ee5b3ea2c6c
Vũ Nguyễn Bá <vunguyenba310703@gmail.com>
2024-09-28 09:41:43 +0700

    Update UserInfoService.java

--- src/main/java/com/example/jwtsecutirydemo/service/UserInfoService.java
+++ src/main/java/com/example/jwtsecutirydemo/service/UserInfoService.java
@@ -1,2 +1,39 @@
-package com.example.jwtsecutirydemo.service;public class UserInfoService {
+package com.example.jwtsecutirydemo.service;
+
+
+import com.example.jwtsecutirydemo.entity.UserInfo;
+import com.example.jwtsecutirydemo.repository.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserInfoService implements UserDetailsService {
+
+    @Autowired
+    private UserInfoRepository repository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserInfo> userDetail = repository.findByName(username); // Assuming 'email' is used as username
+
+        // Converting UserInfo to UserDetails
+        return userDetail.map(UserInfoDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+
+    public String addUser(UserInfo userInfo) {
+        // Encode password before saving the user
+        userInfo.setPassword(encoder.encode(userInfo.getPassword()));
+        repository.save(userInfo);
+        return "User Added Successfully";
+    }
 }
