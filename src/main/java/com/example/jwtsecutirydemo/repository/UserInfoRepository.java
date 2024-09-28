87e637068f62f4f0afa8b196317ed14945e6ccc4
Vũ Nguyễn Bá <vunguyenba310703@gmail.com>
2024-09-28 09:40:58 +0700

    Update UserInfoRepository.java

--- src/main/java/com/example/jwtsecutirydemo/repository/UserInfoRepository.java
+++ src/main/java/com/example/jwtsecutirydemo/repository/UserInfoRepository.java
@@ -1,2 +1,13 @@
-package com.example.jwtsecutirydemo.repository;public class UserInfoRepository {
+package com.example.jwtsecutirydemo.repository;
+
+import com.example.jwtsecutirydemo.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+    Optional<UserInfo> findByName(String name);
 }
